07f991436b107166096d1d8032cf29c5f9857b38
ElioGoat <2434684@cegepba.qc.ca>
2025-02-11 11:40:46 -0500

    je sais pas ce que j'ai fait

---
+++ PO1_Encapsulation/Personnage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO1_Encapsulation
+{
+    public class Personnage
+    {
+        int pointDeVie;
+        int attaque;
+
+        public int PointDeVie {
+            get
+            {
+                return pointDeVie;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    
+                }
+            } 
+        }
+
+        public int Attaque { get => attaque; set => attaque = value; }
+    }
+}
